f0d2551516d9888c05eb948f02146eddf85c209b
LSK <13601152876@163.com>
2024-12-20 22:19:43 +0800

    增加了一些注释

--- tomato.py
+++ tomato.py
@@ -21,28 +21,32 @@ BREAK_MINUTES = 5
 
 def main():
     try:
+        # sys.argv 代表参数列表，当没有提供额外参数时，sys.argv 的长度为1（sys.argv[0] 存储的是被执行的 Python 脚本的文件名）
+        # 执行 ./tomato.py
         if len(sys.argv) <= 1:
             print(f'🍅 tomato {WORK_MINUTES} minutes. Ctrl+C to exit')
             tomato(WORK_MINUTES, 'It is time to take a break')
             print(f'🛀 break {BREAK_MINUTES} minutes. Ctrl+C to exit')
             tomato(BREAK_MINUTES, 'It is time to work')
-
+        # 执行 ./tomato.py -t
         elif sys.argv[1] == '-t':
+            # 区别 ./tomato.py -t 和 ./tomato.py -t <n>
             minutes = int(sys.argv[2]) if len(sys.argv) > 2 else WORK_MINUTES
             print(f'🍅 tomato {minutes} minutes. Ctrl+C to exit')
             tomato(minutes, 'It is time to take a break')
-
+        # 执行 ./tomato.py -b
         elif sys.argv[1] == '-b':
+            # 区别 ./tomato.py -b 和 ./tomato.py -b <n>
             minutes = int(sys.argv[2]) if len(sys.argv) > 2 else BREAK_MINUTES
             print(f'🛀 break {minutes} minutes. Ctrl+C to exit')
             tomato(minutes, 'It is time to work')
-
+        # 执行 ./tomato.py -h
         elif sys.argv[1] == '-h':
             help()
-
+        # 其他情况：表明参数输入错误，直接调用 help()
         else:
             help()
-
+    # 按下 Ctrl+c 会触发 KeyboardInterrupt 异常
     except KeyboardInterrupt:
         print('\n👋 goodbye')
     except Exception as ex:
@@ -64,6 +68,7 @@ def tomato(minutes, notify_msg):
 
         countdown = '{}:{} ⏰'.format(int(left_seconds / 60), seconds_str)
         duration = min(minutes, 25)
+        # 调用 progressbar 函数显示进度条
         progressbar(diff_seconds, minutes * 60, duration, countdown)
         time.sleep(1)
 
@@ -115,8 +120,8 @@ def notify_me(msg):
 def help():
     appname = sys.argv[0]
     appname = appname if appname.endswith('.py') else 'tomato'  # tomato is pypi package
-    print('======  Tomato Clock  ======')
-    print('====== Created By LSK ======')
+    print('====== Tomato Clock ======')
+    print('====== Modified:LSK ======')
     print(f'{appname}         # start a {WORK_MINUTES} minutes tomato clock + {BREAK_MINUTES} minutes break')
     print(f'{appname} -t      # start a {WORK_MINUTES} minutes tomato clock')
     print(f'{appname} -t <n>  # start a <n> minutes tomato clock')
